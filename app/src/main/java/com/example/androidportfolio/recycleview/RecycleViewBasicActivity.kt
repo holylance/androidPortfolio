04d677906591539a4b501ae3e14d989031042466
holylance <holylance82@gmail.com>
2019-05-15 17:33:33 +0200

    parent and children activity

--- app/src/main/java/com/example/androidportfolio/recycleview/RecycleViewBasicActivity.kt
+++ app/src/main/java/com/example/androidportfolio/recycleview/RecycleViewBasicActivity.kt
@@ -1,16 +1,19 @@
 package com.example.androidportfolio.recycleview
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import com.example.androidportfolio.R
 import kotlinx.android.synthetic.main.activity_recycle_view_basic.*
 
 class RecycleViewBasicActivity : AppCompatActivity() {
   companion object {
     const val strTextDetail = "textDetail"
+    const val nRequestTypeNoResult = 1000
   }
 
   inner class ItemClickListener(
@@ -19,7 +22,7 @@ class RecycleViewBasicActivity : AppCompatActivity() {
     override fun onItemClick(item: String) {
       val intent = Intent(context, ListItemActivity::class.java)
       intent.putExtra(strTextDetail, item)
-      startActivity(intent)
+      startActivityForResult(intent, nRequestTypeNoResult)
     }
   }
 
@@ -38,5 +41,17 @@ class RecycleViewBasicActivity : AppCompatActivity() {
     // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
     val adapter = RecycleViewBasicAdapter(list, ItemClickListener(this))
     recyclerView.adapter = adapter
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    when (requestCode) {
+      nRequestTypeNoResult -> if (resultCode == Activity.RESULT_OK) {
+        if (data != null) {
+          Toast.makeText(this, data.getStringExtra("result"), Toast.LENGTH_SHORT).show()
+        }
+      }
+    }
   }
 }
