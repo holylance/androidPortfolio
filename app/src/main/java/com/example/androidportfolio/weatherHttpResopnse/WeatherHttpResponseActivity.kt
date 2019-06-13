08d68737fc24a430a6ce0e87cf01551862c48ebc
holylance <holylance82@gmail.com>
2019-06-13 18:08:23 +0200

    complete rxkotlin and rxandroid

--- app/src/main/java/com/example/androidportfolio/weatherHttpResopnse/WeatherHttpResponseActivity.kt
+++ app/src/main/java/com/example/androidportfolio/weatherHttpResopnse/WeatherHttpResponseActivity.kt
@@ -6,12 +6,12 @@ import android.widget.TextView
 import com.example.androidportfolio.R
 import com.example.androidportfolio.base.BaseActivity
 import com.example.androidportfolio.network.RetrofitServiceGenerator
-import com.example.androidportfolio.network.WeatherData
 import com.example.androidportfolio.network.WeatherService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+
 import kotlinx.android.synthetic.main.activity_weather_http_response.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class WeatherHttpResponseActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,14 +26,12 @@ class WeatherHttpResponseActivity : BaseActivity() {
   private fun setRetrofitService(v: TextView) {
     val service = RetrofitServiceGenerator.createService(WeatherService::class.java)
     val request = service.getCurrentWeather(RetrofitServiceGenerator.strKey, "Berlin,de")
-    request.enqueue(object : Callback<WeatherData> {
-      override fun onResponse(call: Call<WeatherData>, response: Response<WeatherData>) {
-        v.text = response.body().toString()
-      }
-
-      override fun onFailure(call: Call<WeatherData>, t: Throwable) {
-        Log.d("ERROR", t.toString())
-      }
-    })
+    val result = request.subscribeOn(Schedulers.io())
+      .observeOn(AndroidSchedulers.mainThread())
+      .subscribeBy (
+        onNext = { v.text = it.toString() },
+        onError = { Log.d("ERROR", it.printStackTrace().toString()) },
+        onComplete = { Log.d("COMPLETE", "Done") }
+      )
   }
 }
