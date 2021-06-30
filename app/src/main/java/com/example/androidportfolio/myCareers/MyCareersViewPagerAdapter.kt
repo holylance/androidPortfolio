8ba90b8e0167c9712ed961186c5ca710921fee44
holylance <holylance82@gmail.com>
2021-06-30 20:33:17 +0200

    Feature: My Career fragment

--- app/src/main/java/com/example/androidportfolio/myCareers/MyCareersViewPagerAdapter.kt
+++ app/src/main/java/com/example/androidportfolio/myCareers/MyCareersViewPagerAdapter.kt
@@ -9,32 +9,32 @@ import androidx.viewpager.widget.PagerAdapter
 import com.example.androidportfolio.R
 
 class MyCareersViewPagerAdapter(
-  private var mContext: Context
-): PagerAdapter() {
-  override fun instantiateItem(container: ViewGroup, position: Int): Any {
-    val view: View?
+    private var mContext: Context
+) : PagerAdapter() {
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view: View?
 
-    val inflater = mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-    view = inflater.inflate(R.layout.page_v, container, false)
+        val inflater = mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        view = inflater.inflate(R.layout.page_v, container, false)
 
-    val textView = view!!.findViewById(R.id.title) as TextView
-    textView.text = "TEXT $position"
+        val textView = view!!.findViewById(R.id.title) as TextView
+        textView.text = "TEXT $position"
 
-    // 뷰페이저에 추가.
-    container.addView(view)
+        // 뷰페이저에 추가.
+        container.addView(view)
 
-    return view
-  }
+        return view
+    }
 
-  override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-    container.removeView(`object` as View)
-  }
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
 
-  override fun getCount(): Int {
-    return 10
-  }
+    override fun getCount(): Int {
+        return 10
+    }
 
-  override fun isViewFromObject(view: View, `object`: Any): Boolean {
-    return view === `object` as View
-  }
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object` as View
+    }
 }
