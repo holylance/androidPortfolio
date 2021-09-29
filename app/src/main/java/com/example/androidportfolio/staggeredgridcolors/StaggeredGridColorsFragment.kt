b040486ba4bf8f29b94751627d0ee91e71bd3c6d
holylance <holylance82@gmail.com>
2021-09-29 16:23:34 +0200

    Feature: Make Staggered Grid Colors Fragment

--- app/src/main/java/com/example/androidportfolio/staggeredgridcolors/StaggeredGridColorsFragment.kt
+++ app/src/main/java/com/example/androidportfolio/staggeredgridcolors/StaggeredGridColorsFragment.kt
@@ -1,18 +1,18 @@
 package com.example.androidportfolio.staggeredgridcolors
 
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.androidportfolio.R
-import com.example.androidportfolio.base.BaseActivity
-import com.example.androidportfolio.databinding.ActivityStaggeredGridColorsBinding
+import com.example.androidportfolio.base.BaseFragment
+import com.example.androidportfolio.databinding.StaggeredGirdColorsFragmentBinding
 import com.example.androidportfolio.util.viewBinding
 
-class StaggeredGridColorsActivity : BaseActivity(R.layout.activity_staggered_grid_colors) {
+class StaggeredGridColorsFragment : BaseFragment(R.layout.staggered_gird_colors_fragment) {
+    private val binding by viewBinding(StaggeredGirdColorsFragmentBinding::bind)
 
-    private val binding by viewBinding(ActivityStaggeredGridColorsBinding::bind)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val list = listOf(
             "A",
@@ -38,5 +38,10 @@ class StaggeredGridColorsActivity : BaseActivity(R.layout.activity_staggered_gri
         binding.staggeredGridColor.layoutManager = staggeredGridLayoutManager
         val adapter = StaggeredGridColorsAdapter(list)
         binding.staggeredGridColor.adapter = adapter
+    }
+
+    override fun onDestroyView() {
+        binding.staggeredGridColor.adapter = null
+        super.onDestroyView()
     }
 }
